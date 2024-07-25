ac637497eaa6c06e540c82f6943b931e15c2bbc0
Tyler Leishman <tyler.leishman0@gmail.com>
2024-07-25 07:09:34 -0500

    third round review fixes

--- routes/users.js
+++ routes/users.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { getCurrentUser, updateUser } = require('../controllers/users');
-const { auth } = require('../middleware/auth');
+const auth = require('../middleware/auth');
 const { validateUserUpdate } = require('../middleware/validation');
 
 router.get('/me', auth, getCurrentUser);
